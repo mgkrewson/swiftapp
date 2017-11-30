eac0cd4c52c53239ce6bda298f538e37fef4ec62
mgkrewson <mgkrewson@gmail.com>
2017-11-29 18:42:16 -0600

    Now we can add numbers

--- First App/ViewController.swift
+++ First App/ViewController.swift
@@ -18,8 +18,7 @@ class ViewController: UIViewController {
     
 
     @IBAction func buttonPressed(_ sender: Any) {
-        print(text1.text)
-        print(text2.text)
+        theLabel.text = "answer: \(Double(text1.text!)! + Double(text2.text!)!)"
     }
 
     
